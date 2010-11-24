7144accccc21c8981754affe0e4dc54aef8cfd9b
Simon King <simon@organicinternet.co.uk>
2010-11-24 13:10:26 +0000

    Helps if you require the file...

--- lib/rvideo.rb
+++ lib/rvideo.rb
@@ -28,6 +28,7 @@ require 'rvideo/tools/qtfaststart'
 require 'rvideo/tools/segmenter'
 require 'rvideo/tools/handbrakecli'
 require 'rvideo/tools/lame'
+require 'rvideo/tools/neroaacenc'
 
 TEMP_PATH   = File.expand_path(File.dirname(__FILE__) + '/../tmp')
 REPORT_PATH = File.expand_path(File.dirname(__FILE__) + '/../report')
